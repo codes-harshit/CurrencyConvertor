e4a72266f09a6dd23c7c6b4de8e70b4955b6fd70
codes-harshit <programming.harshit44@gmail.com>
2024-06-02 18:54:56 +0530

    Resolve from currency issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
 import React, { useEffect } from "react";
 import { InputBox } from "./components";
 import { useState } from "react";
-import useCurrencyInfo from "./CustomHooks/useCurrencyInfo";
+// import useCurrencyInfo from "./CustomHooks/useCurrencyInfo";
 
 function App() {
+  function useCurrencyInfo(currency) {
+    let [data, setData] = useState({});
+
+    useEffect(() => {
+      fetch(
+        `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+      )
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]));
+    }, [currency]);
+
+    return data;
+  }
+
   let [amount, setAmount] = useState();
   let [convertedAmount, setConvertedAmount] = useState(0);
   let [from, setFrom] = useState("usd");
   let [to, setTo] = useState("inr");
-  let options = [];
-  let currencyInfo = {};
 
-  // useEffect(() => {
-  //   currencyInfo = useCurrencyInfo(from);
-  //   options = Object.keys(currencyInfo)
-  // }, [from])
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
 
-  currencyInfo = useCurrencyInfo(from);
-  options = Object.keys(currencyInfo);
+  let currencyInfo = useCurrencyInfo(from);
+  let options = Object.keys(currencyInfo);
 
   let convert = () => {
     setConvertedAmount((amount * currencyInfo[to]).toFixed(4));
   };
 
+  // useEffect(() => {
+  //   convert();
+  // }, [amount, from, to]);
+
   return (
-    <div 
-    className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-        style={{
-            backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-        }}>
-      <div  className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
+      }}
+    >
+      <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -48,10 +67,13 @@ function App() {
             />
           </div>
           <div className="relative w-full h-0.5">
-            <button 
-            type="button"
-            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
-            >Swap</button>
+            <button
+              type="button"
+              className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+              onClick={swap}
+            >
+              Swap
+            </button>
           </div>
           <div className="w-full mt-1 mb-4">
             <InputBox
@@ -63,7 +85,10 @@ function App() {
               selectCurrency={to}
             />
           </div>
-          <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg" >
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+          >
             Convert {from.toUpperCase()} to {to.toUpperCase()}
           </button>
         </form>
--- src/CustomHooks/useCurrencyInfo.js
+++ src/CustomHooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency){
     let [data, setData] = useState({});
 
     useEffect(() => {
-        fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json')
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]));
 
